63306e6a1a4b71391a71e791be98b7fa0e3015f0
igorsousap <igorsousapinto140@gmail.com>
2025-02-09 19:10:47 -0300

    adds mangament for qr_code

--- lib/full_plate/service/order_service.ex
+++ lib/full_plate/service/order_service.ex
@@ -3,19 +3,30 @@ defmodule FullPlate.Service.OrderService do
 
   alias FullPlate.Products
   alias FullPlate.Orders
+  alias FullPlate.MercadoPago.QrCode
 
   def create_order(order) do
-   total = order["order"]
-    |> calculate_total_price()
+   items = order["order"]
+    |> calculate_total_price() |> IO.inspect()
 
    final_order =
     order
-    |> Map.put("total", total)
+    |> Map.put("total", items.total_amount)
 
     case Orders.create_order(final_order) do
       {:ok, order} ->
-        Logger.info("Order created")
-        {:ok, order}
+
+        order
+        |> create_body_qr_code(items)
+        |> IO.inspect()
+        |> QrCode.create_qr()
+        |> case do
+          {:ok, response} ->
+            Orders.update_order(order.id, %{qr_code: response["qr_data"]})
+
+          error ->
+            error
+        end
 
       error ->
         Logger.error(
@@ -53,20 +64,72 @@ defmodule FullPlate.Service.OrderService do
     end
   end
 
-  def update_order(params) do
-    case Orders.update_order(params) do
+  def update_order(id, params) do
+    case Orders.update_order(id, params) do
       nil -> {:error, :not_found}
 
       {:ok, order} -> order
     end
   end
 
-  defp calculate_total_price(order) do
-  total = Enum.reduce(order, Decimal.new(0), fn id, acc ->
-     product = Products.get_by_id(id)
-     Decimal.add(acc, product.price)
+  def calculate_total_price(order) do
+    items = Enum.reduce(order, %{}, fn id, acc ->
+      product = Products.get_by_id(id)
+
+      acc
+      |> Map.update(product.product_name, %{
+           title: product.product_name,
+           description: product.description || "",
+           quantity: 1,
+           unit_price: ensure_float(product.price),
+           unit_measure: "Unit"
+         }, fn existing_item ->
+           %{
+             existing_item |
+             quantity: existing_item.quantity + 1
+           }
+         end)
     end)
 
-    Decimal.to_string(total)
+    # Atualizar o total_amount em cada item
+    updated_items = Map.values(items) |> Enum.map(fn item ->
+      total_amount = Float.round(item.quantity * item.unit_price, 2)
+      Map.put(item, :total_amount, total_amount)
+    end)
+
+    # Calcular o total geral
+    total = Enum.reduce(updated_items, 0.0, fn item, acc ->
+      acc + item.total_amount
+    end)
+
+    %{
+      total_amount: Float.round(total, 2),
+      items: updated_items
+    }
+  end
+
+  defp ensure_float(value) when is_float(value), do: value
+  defp ensure_float(value) when is_integer(value), do: value * 1.0
+  defp ensure_float(value) when is_binary(value), do: String.to_float(value)
+  defp ensure_float(%Decimal{} = value), do: Decimal.to_float(value)
+  defp ensure_float(_), do: 0.0
+
+
+  defp create_body_qr_code(order, items) do
+    total =
+      order.total
+      |> Decimal.to_float()
+      |> Float.round(2)
+
+    %{
+      external_reference: order.id,
+      title: "Pagamento no QR",
+      description: "Compra de produtos",
+      total_amount: total,
+      items: items.items,
+      cash_out: %{
+        amount: 0
+      }
+    }
   end
 end
--- setup_minikube.sh
+++ setup_minikube.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-# Passo 01: Start Minikube e configurar o contexto
 echo "Iniciando Minikube..."
 minikube start
 kubectl config use-context minikube
 
-# Passo 02: Configurar Docker localmente e habilitar addons
 echo "Configurando Docker localmente com Minikube..."
 eval $(minikube docker-env)
 
@@ -13,38 +11,37 @@ echo "Habilitando addons do Minikube..."
 minikube addons enable metallb
 minikube addons enable metrics-server
 
-# Passo 03: Build da imagem Docker
 echo "Construindo a imagem Docker..."
 docker build -t full-plate:v1 .
 
-# Passo 04: Aplicar postgres.yaml e deployment.yaml
 echo "Aplicando postgres.yaml e deployment.yaml..."
 kubectl apply -f postgres.yaml
 kubectl apply -f deployment.yaml
 
-# Passo 05: Aplicar service.yaml
+echo "Aguardando os pods ficarem prontos..."
+while [[ $(kubectl get pods --no-headers | grep -c "0/1") -gt 0 ]]; do
+  echo "Ainda aguardando os pods ficarem prontos..."
+  sleep 5
+done
+echo "Todos os pods estão prontos!"
+
 echo "Aplicando service.yaml..."
 kubectl apply -f service.yaml
 
-# Passo 06: Aplicar secret.yaml
 echo "Aplicando secret.yaml..."
 kubectl apply -f secret.yaml
 
-# Passo 07: Aplicar configmap.yaml
 echo "Aplicando configmap.yaml..."
 kubectl apply -f configmap.yaml
 
-# Passo 08: Aplicar hpa.yaml
 echo "Aplicando hpa.yaml..."
 kubectl apply -f hpa.yaml
 
-# Passo 09: Validar objetos criados
 echo "Validando objetos criados..."
 kubectl get pods
 kubectl get svc
 kubectl get hpa
 
-# Passo 10: Obter o endpoint do serviço
 echo "Obtendo o endpoint do serviço..."
 minikube service full-plate-service --url
 
